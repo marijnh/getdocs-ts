60892af0004019d632e44ed9a8decfc415f6f7fa
Adrian Heine <mail@adrianheine.de>
2019-06-26 10:22:28 +0200

    Take comment from either accessor
    We could also merge them.

--- src/index.js
+++ src/index.js
@@ -294,13 +294,17 @@ class Gatherer {
             const [name, v] = ret
             props = props || Object.create(null)
             if (!props[name]) props[name] = {...v.returns, loc: v.loc, id: v.id, readonly: true, description: v.description}
+            else props[name].description = props[name].description || v.description
           } else if (ts.SyntaxKind[prop.kind] == "SetAccessor") {
             const ret = this.callHandler(prop, ctx)
             if (!ret) continue
             const [name, v] = ret
             props = props || Object.create(null)
             if (!props[name]) props[name] = {...v.returns, loc: v.loc, id: v.id, description: v.description}
-            delete props[name].readonly
+            else {
+              props[name].description = props[name].description || v.description
+              delete props[name].readonly
+            }
           } else {
             props = this.handleNode(prop, ctx, props)
           }
