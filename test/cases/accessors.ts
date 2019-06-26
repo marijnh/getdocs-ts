df4d428f2ee0a05ecf4742f26ae61a9e97730fed
Adrian Heine <mail@adrianheine.de>
2019-06-26 10:23:59 +0200

    Add comments to accessor test

--- test/cases/accessors.ts
+++ test/cases/accessors.ts
@@ -1,9 +1,14 @@
 let y = 0
 export class X {
+  // This is get x
   get x() { return 5 }
+  // This is get y
   get y() { return y }
+  // This is set y
   set y(newY: number) { y = newY }
+  // This is static get x
   static get x() { return 5 }
   static get y() { return y }
+  // This is static set y
   static set y(newY: number) { y = newY }
 }
