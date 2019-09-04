marijnh/getdocs-ts

commit b1108af9d1a4d44e4555c366f1bf6c5c44c746f9

Fix issue with references to reexported types

src/index.ts
@@ -476,8 +476,15 @@ export function gather({filename, items = Object.create(null)}: {filename: strin
   const tc = program.getTypeChecker()
   const exports = tc.getExportsOfModule(tc.getSymbolAtLocation(program.getSourceFile(filename)!)!)
   const basedir = resolve(dirname(configPath || filename))
-  const module = new Context(tc, exports, basedir, "", [])
-  module.gatherSymbols(exports, items, "")
-  
+
+  // Add all symbols aliased by exports to the set of things that
+  // should be considered exported
+  const closedExports = exports.slice()
+  for (let i = 0; i < closedExports.length; i++) {
+    let sym = closedExports[i], alias = (sym.flags & SymbolFlags.Alias) ? tc.getAliasedSymbol(sym) : null
+    if (alias && !closedExports.includes(alias)) closedExports.push(alias)
+  }
+
+  new Context(tc, closedExports, basedir, "", []).gatherSymbols(exports, items, "")
   return items
 }

commit acad53a5f7456a8564eb715aeba8ca3a1233fd9a

Fix a bug introduced in b1108af9d1a4d

src/index.ts
@@ -10,7 +10,7 @@ import {
   Declaration, NamedDeclaration, TypeParameterDeclaration, ParameterDeclaration, EnumDeclaration, VariableDeclaration, ConstructorDeclaration
 } from "typescript"
 
-const {resolve, dirname, relative} = require("path")
+const {resolve, dirname, relative, sep} = require("path")
 
 type BindingKind = "class" | "enum" | "enummember" | "interface" | "variable" | "property" | "method" |
   "typealias" | "typeparam" | "constructor" | "function" | "parameter" | "reexport"
@@ -399,7 +399,7 @@ class Context {
     let decl = maybeDecl(symbol)
     if (!decl) return true
     let path = resolve(decl.getSourceFile().fileName)
-    return !path.startsWith(this.basedir + path.sep) || /\bnode_modules\b/.test(path.slice(this.basedir.length))
+    return !path.startsWith(this.basedir + sep) || /\bnode_modules\b/.test(path.slice(this.basedir.length))
   }
 }
 